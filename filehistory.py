288e71e90b3d32853289bd0ca4b38c38d01fffe5
Roy Solberg <roy.solberg@evry.com>
2015-01-18 19:11:46 +0100

    WIP with script.

--- filehistory.py
+++ filehistory.py
@@ -1,12 +1,11 @@
+import spacemonkeyconfig
 import sqlite3
 import os
 import sys
 import hashlib
 import datetime
-
-SPACE_MONKEY_PATH = '/Users/roy/SpaceMonkey'
-SPACE_MONKEY_PATH = '/Users/roy/SpaceMonkey/Photos/2012/2012-01-11-Ullplagg'
-SQLITE_FILE_PATH = '/Users/roy/Google Drive/spacemonkey-filehistory.sql'
+import smtplib
+import email.mime.text
 
 def md5_for_file(path, block_size=256*128, hr=False):
     '''
@@ -22,41 +21,51 @@ def md5_for_file(path, block_size=256*128, hr=False):
         return md5.hexdigest()
     return md5.digest()
 
-def main():
-	print('Using Space Monkey path [%s].' % (SPACE_MONKEY_PATH))
-	print('Using SQLite file path [%s].' % (SQLITE_FILE_PATH))
+def send_mail(contents):
+	mail = email.mime.text.MIMEText(contents)
+	mail['Subject'] = 'Space Monkey file report'
+	mail['From'] = spacemonkeyconfig.EMAIL_ADDRESS_FROM
+	mail['To'] = spacemonkeyconfig.EMAIL_ADDRESS_TO
 
-	if not os.path.exists(SPACE_MONKEY_PATH):
-		print ('Could not find Space Monkey path. Terminating program.')
+def main():
+	print('Using Space Monkey path [%s].' % (spacemonkeyconfig.SPACE_MONKEY_PATH))
+	print('Using SQLite file path [%s].' % (spacemonkeyconfig.SQLITE_FILE_PATH))
+
+	if not os.path.exists(spacemonkeyconfig.SPACE_MONKEY_PATH):
+		print ('Could not find Space Monkey path. Reporting and terminating program.')
 		sys.exit(1)
 
 	isFirstRun = False
 
-	if not os.path.isfile(SQLITE_FILE_PATH):
+	if not os.path.isfile(spacemonkeyconfig.SQLITE_FILE_PATH):
 		print 'SQLite file doesn\' exist. File will be created.'
 		isFirstRun = True
 
-	connection = sqlite3.connect(SQLITE_FILE_PATH)
+	send_mail('yay :)')
+	connection = sqlite3.connect(spacemonkeyconfig.SQLITE_FILE_PATH)
 	cursor = connection.cursor()
-	cursor.execute('CREATE TABLE IF NOT EXISTS files(id INTEGER PRIMARY KEY, name TEXT UNIQUE, size INTEGER, hash TEXT, createdDateTime DATETIME, modifiedDateTime DATETIME, addedDateTime DATETIME, updatedDateTime DATETIME)')
+	cursor.execute('CREATE TABLE IF NOT EXISTS files(id INTEGER PRIMARY KEY, name TEXT UNIQUE, deleted BOOLEAN DEFAULT FALSE, size INTEGER, hash TEXT, created DATETIME, modified DATETIME, added DATETIME, hash_change DATETIME, last_seen)')
 
 	now = datetime.datetime.now()
 
-	for root, dirs, files in os.walk(SPACE_MONKEY_PATH):
+	for root, dirs, files in os.walk(spacemonkeyconfig.SPACE_MONKEY_PATH):
 		for name in files:
 			path = os.path.join(root, name)
 			name = path[len(SPACE_MONKEY_PATH):].decode('utf8')
-			size = os.path.getsize(path)
 			md5 = md5_for_file(path, 256*128, True)
-			created = datetime.datetime.fromtimestamp(os.path.getctime(path))
-			modified = datetime.datetime.fromtimestamp(os.path.getmtime(path))
 			cursor.execute('SELECT hash FROM files WHERE name = ?', (name, ))
 			row = cursor.fetchone()
-			if row == None: # New file
-				cursor.execute('INSERT INTO files(name, size, hash, createdDateTime, modifiedDateTime, addedDateTime, updatedDateTime) VALUES(?, ?, ?, ?, ?, ?, ?)', (name, size, md5, created, modified, now, now))
-			elif row[0] != md5: # File contents has changed
-				cursor.execute('UPDATE files SET size = ?, hash = ?, modifiedDateTime = ?, updatedDateTime = ? WHERE name = ?', (size, md5, modified, now, name))
-	
+			if row == None or row[0] != md5: # New or changed file
+				size = os.path.getsize(path)
+				created = datetime.datetime.fromtimestamp(os.path.getctime(path))
+				modified = datetime.datetime.fromtimestamp(os.path.getmtime(path))
+				if row == None: # New file
+					cursor.execute('INSERT INTO files(name, size, hash, created, modified, added, hash_change, last_seen) VALUES(?, ?, ?, ?, ?, ?, ?, ?)', (name, size, md5, created, modified, now, now, now))
+				elif row[0] != md5: # File contents has changed
+					cursor.execute('UPDATE files SET size = ?, hash = ?, modified = ?, hash_change = ?, last_seen = ? WHERE name = ?', (size, md5, modified, now, now, name))
+			else:
+				cursor.execute('UPDATE files SET last_seen = ? WHERE name = ?', (now, name))
+
 	connection.commit()
 	connection.close()
 
